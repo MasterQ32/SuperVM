7dd39e1c0d7ef5aefceb1660fca489f45ead2619
Felix Queißner <git@mq32.de>
2016-07-08 18:00:32 +0200

    Creates examples of new assembler features.

--- data/timer-test.asm
+++ data/timer-test.asm
@@ -1,25 +1,32 @@
-jmp @start
-jmp @int_timer ; 1
-ret ; 2
-ret ; 3
-ret ; 4
-ret ; 5
-ret ; 6
-ret ; 7
-ret ; 8
+; includes a memory specification file
+:INCLUDE "timer-test.mem"
+
+; entry point
+:ORIGIN 0x0000
+	jmp @start
+
+	; interrupt list
+	jmp @int_timer ; 1
+	ret ; 2
+	ret ; 3
+	ret ; 4
+	ret ; 5
+	ret ; 6
+	ret ; 7
+	ret ; 8
 
 start:
-	push 0x1000
+	push $strFreq
 	cpget
 	jmp @puts
 	drop
 	
-	in 0x10000 ; Timer Frequency
+	in 0x10000
 	[i0:peek] syscall [ci:2]
 	
 	out 0x10002 ; Timer Limit
 	
-	push 0x1012 ; " Hz\n"
+	push $strFreq 
 	cpget
 	jmp @puts
 	drop
@@ -33,12 +40,12 @@ mainloop:
 
 int_timer:
 	
-	load32 0x101F
+	load32 $iCounter
 	[i0:peek] syscall [ci:2]
 	[i0:arg] add 1
-	store32 0x101F
+	store32 $iCounter
 	
-	push 0x1017 ; "tick\n"
+	push $strTicks
 	cpget
 	jmp @puts
 
