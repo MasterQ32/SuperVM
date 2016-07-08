d98619be3d1b82b3fdd67c9ee236a7f51e06faed
Felix Queißner <git@mq32.de>
2016-07-08 11:34:21 +0200

    Implements some simple assembler examples.

---
+++ data/runtime.asm
@@ -0,0 +1,23 @@
+
+
+
+
+;
+; void puts(char *sz)
+;
+puts:
+	bpget
+	spget
+	bpset
+	get -2 ; sz
+puts_loop:
+	[i0:peek] load8i [f:yes]
+	[ex(z)=1] jmp @puts_end
+	[i0:pop] syscall [ci:1]
+	[i0:arg] add 1 
+	jmp @puts_loop
+puts_end:
+	bpget
+	spset
+	bpset
+	ret
---
+++ data/timer-test.asm
@@ -0,0 +1,40 @@
+jmp @start
+jmp @int_timer ; 1
+ret ; 2
+ret ; 3
+ret ; 4
+ret ; 5
+ret ; 6
+ret ; 7
+ret ; 8
+
+start:
+	push 0x1000
+	cpget
+	jmp @puts
+	drop
+	
+	in 0x10000 ; Timer Frequency
+	[i0:peek] syscall [ci:2]
+	
+	out 0x10002 ; Timer Limit
+	
+	push 0x1012 ; " Hz\n"
+	cpget
+	jmp @puts
+	drop
+	
+	sei
+	
+mainloop:
+	jmp @mainloop
+	
+	syscall
+
+int_timer:
+	
+	push 0x1017 ; "tick\n"
+	cpget
+	jmp @puts
+
+	ret
